888a6d1a2581b4e4411583ed0360b46bc20a81f9
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-10-23 01:17:57 +0300

    feat: add solution to brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let strArr = str.split('');
+    for (let i = 0; i < strArr.length; i++) {
+        let strSymb = strArr[i];
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let configPair = bracketsConfig[j];
+            if (stack.length == 0 && strSymb == configPair[0]) {
+                stack.push(configPair[0]);
+            } else if (strSymb == configPair[0] && stack[stack.length - 1] != configPair[1]) {
+                stack.push(configPair[0]);
+            } else if (strSymb == configPair[1]) {
+                if (stack[stack.length - 1] == configPair[0]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length == 0 ? true : false;
 }
